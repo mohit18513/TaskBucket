f08ecf5e50eca6df4733c4250eeb1c431fcf4929
Sachin <ssachinmmehta@gmail.com>
2019-06-13 17:02:40 +0530

    Push test

---
+++ TaskBucketJava/src/main/java/com/tk20/ctuoprestapi/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package main.java.com.tk20.ctuoprestapi.config;
+
+
+//@EnableWebSecurity
+//@Configuration
+public class SecurityConfiguration {
+
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//
+//        httpSecurity
+//                .authorizeRequests()
+//                .anyRequest()
+//                .permitAll();
+//        httpSecurity.csrf().disable();
+//
+//    }
+    
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//            .withUser("admin").password(encoder().encode("adminPass")).roles("ADMIN")
+//            .and()
+//            .withUser("user").password(encoder().encode("userPass")).roles("USER");
+//    }
+//     
+//    @Bean
+//    public PasswordEncoder  encoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+//    @Bean
+//    public CustomFilter customFilter() {
+//        return new CustomFilter();
+//    }
+}
---
+++ TaskBucketJava/src/test/java/com/tk20/ctuoprestapi/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,12 @@
+package test.java.com.tk20.ctuoprestapi;
+
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SpringSecurityExampleApplicationTests {
+
+//	@Test
+	public void contextLoads() {
+	}
+
+}
