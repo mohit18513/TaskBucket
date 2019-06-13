afa51b824301905d60fe479571da5636c763c0a7
manishsharma <manishsharma@WM0010.TSTK.local>
2019-06-13 19:51:25 +0530

    GET User API

--- TaskBucketJava/src/main/java/com/tk20/Entities/Task.java
+++ TaskBucketJava/src/main/java/com/tk20/Entities/Task.java
@@ -1,5 +1,7 @@
 package main.java.com.tk20.Entities;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
 
 public class Task {
@@ -33,5 +35,54 @@ public class Task {
 	private String title;
 
 	private String description;
+
+	private Date due_date;
+
+	public Date getDue_date() {
+		return due_date;
+	}
+
+	public void setDue_date(Date due_date) {
+		this.due_date = due_date;
+	}
+
+	public Date getCreated_by() {
+		return created_by;
+	}
+
+	public void setCreated_by(Date created_by) {
+		this.created_by = created_by;
+	}
+
+	public Integer getOwner() {
+		return Owner;
+	}
+
+	public void setOwner(Integer owner) {
+		Owner = owner;
+	}
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+	public Timestamp getLast_commented_on() {
+		return last_commented_on;
+	}
+
+	public void setLast_commented_on(Timestamp last_commented_on) {
+		this.last_commented_on = last_commented_on;
+	}
+
+	private Date created_by;
+
+	private Integer Owner;
+	private String Status;
+
+	private Timestamp last_commented_on;
 
 }
